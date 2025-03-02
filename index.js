1fad7ff5313bae329df41f43b88122d46594909a
DrunkPiano <paulwu@DrunkPianos-MacBook-Pro.local>
2025-03-03 06:17:06 +1100

    my commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const input = document.getElementById("teminput");
+const outputlabel = document.getElementById("outputlabel");
+const toFah = document.getElementById("toFah");
+const toCel = document.getElementById("toCel");
+let temp;
+
+
+function convert(){
+    temp = Number(input.value);
+    if(toFah.checked){
+        temp = temp * 9 / 5 + 32;
+        outputlabel.textContent =  temp.toFixed(1) + "°F";
+    }else if(toCel.checked){
+        temp = (temp - 32) * (5 / 9);
+        outputlabel.textContent = temp.toFixed(1) + "°C";
+    }else{
+        outputlabel.textContent = "Select one unit";
+    }
+}
